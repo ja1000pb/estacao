92d6d4f95f5ffba3219f23357c88a41f113f7d55
ja1000pb <ja1000pb@gmail.com>
2026-08-17 00:32:21 -0300

    v2

---
+++ src/main/resources/db/migration/V4__altera_tabela.sql
@@ -0,0 +1,4 @@
+    alter table estacao 
+       add column latitude varchar(100),
+       add column longitude varchar(100);     
+    
--- src/main/resources/db/migration/V4_alter_tables.sql
+++
@@ -1,4 +0,0 @@
-    alter table estacao add column(
-        latitude varchar(100),
-        longitude varchar(100)       
-    )
